638633b408268562fd64df8b820a18298ed42d07
Todd <thickam@outlook.com>
2023-10-04 22:21:41 -0700

    added grid card template from bootstrap

--- react_client/src/components/card/Card.jsx
+++ react_client/src/components/card/Card.jsx
@@ -1,9 +1,27 @@
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 export default function ComponentOne() {
   return (
     <>
-      <div className="card-container text-center mt-3">
-        <h1>Card</h1>
-      </div>
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col key={idx}>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
   );
 }
