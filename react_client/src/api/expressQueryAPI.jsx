c6e16e8a3b3336981b777829cbcad90b340c0c20
Todd <thickam@outlook.com>
2023-10-07 14:40:19 -0700

    saving

--- react_client/src/api/expressQueryAPI.jsx
+++ react_client/src/api/expressQueryAPI.jsx
@@ -1,30 +1,49 @@
 import axios from "axios";
 
-// Function could be updated to Async/Await with try/catch
-// or using new Promise((resolve,reject))
-
 /*
   Post request to 'get' one of the following queries:
   'purchases', 'sales', 'remaining', 'exchanges', or 'assets' 
 
   Server Request Path: '/express_server/controllers/queriesController.js'
 */
-export function expressQueryAPI(query, setData, setIsData, setAnimation) {
-  axios
-    .post("http://localhost:5500/queries", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        query: query,
-      },
-    })
-    .then((response) => {
-      setData(response.data);
-      setIsData(true);
-      setAnimation(false);
-    })
-    .catch((error) => {
-      console.error(`Error: unable to fetch ${query} from server`, error);
-    });
+
+// const [myData, setmyData] = useState([]);
+// const [isData, setIsData] = useState(false);
+// const [isAnimation, setisAnimation] = useState(false);
+
+// return (
+//   {isAnimation && <MyAnimation />}
+//   {
+
+//     isData && <Mycomponent />
+//   }
+// )
+
+export default async function expressQueryAPI(
+  query,
+  setData,
+  setIsData,
+  setAnimation
+) {
+  try {
+    await axios
+      .post("http://localhost:5500/queries", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          query: query,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+        setIsData(true);
+        setAnimation(false);
+      })
+      .catch((error) => {
+        console.error(`Error: unable to fetch ${query} from server`, error);
+      });
+  } catch (error) {
+    console.error(error.message);
+  }
 }
