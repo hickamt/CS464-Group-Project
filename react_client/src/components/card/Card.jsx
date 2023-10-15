571df899c2ed48b0d5b13e5a66ada39bdff22c34
Todd <thickam@outlook.com>
2023-10-15 16:49:38 -0700

    added spin animation from react-loading library for Card view

--- react_client/src/components/card/Card.jsx
+++ react_client/src/components/card/Card.jsx
@@ -6,12 +6,24 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import { useEffect, useState } from "react";
 import expressQueryAPI from "../../api/expressQueryAPI";
-// import react animation bars
-
+import ReactLoading from "react-loading";
 // CSS Styles
 import "./styles/card.css";
 
-// Fetch crypto icon using 'asset' as the asset name
+// Remember to abstract spin animation to module file
+const spinAnimation = function reactSpinLoadingAnimation() {
+  return (
+    <ReactLoading
+      className="mx-auto"
+      type="spin"
+      color="#1E762E"
+      height={100}
+      width={100}
+    />
+  );
+};
+
+// Fetch crypto icon using 'asset' as the asset name (abstract this function)
 const getCryptoIcon = function fetchCryptoImagePngIcon(asset) {
   return `https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/${asset}.png`;
 };
@@ -19,7 +31,7 @@ const getCryptoIcon = function fetchCryptoImagePngIcon(asset) {
 export default function ComponentOne() {
   const [assetData, setAssetData] = useState([]);
   const [isData, setIsData] = useState(false); // [false, true]
-  const [animation, setAnimation] = useState(false);
+  const [animation, setAnimation] = useState(true); // [false, true
 
   // call expressQueryAPI() to fetch data from server once
   useEffect(() => {
@@ -29,45 +41,51 @@ export default function ComponentOne() {
 
   return (
     <>
-      {isData && (
-        <>
-          <h1 className="card-title d-none">Crypto Assets</h1>
-          <Row className="media-row d-flex flex-nowrap overflow-scroll">
-            {assetData.map((data, index) => (
-              <Card key={index} className="media-card ">
-                <Card.Img
-                  className="card-img mx-auto"
-                  alt="cryptocurrency"
-                  variant="top"
-                  src={getCryptoIcon(data.asset)}
-                />
-                <Card.Body className="">
-                  <div className="card-grid">
-                    <div className="value-type mx-auto">
-                      <p className="card-text">Asset</p>
-                      <p className="card-text">Qty</p>
-                      <p className="card-text">Spot</p>
-                      <p className="card-text">Value</p>
-                      <p className="card-text">Day</p>
-                      <p className="card-text">Week</p>
-                      <p className="card-text">Month</p>
-                    </div>
-                    <div className="value mx-auto">
-                      <p className="card-text">{data.asset.toUpperCase()}</p>
-                      <p className="card-text">{Number.parseFloat(data.remaining).toFixed(3)}</p>
-                      <p className="card-text">$ 1.12</p>
-                      <p className="card-text">$ 2.24</p>
-                      <p className="card-text">1.32%</p>
-                      <p className="card-text">0.32%</p>
-                      <p className="card-text">0.01%</p>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
-            ))}
-          </Row>
-        </>
-      )}
+      {animation
+        ? spinAnimation()
+        : isData && (
+            <>
+              <h1 className="card-title d-none">Crypto Assets</h1>
+              <Row className="media-row d-flex flex-nowrap overflow-scroll">
+                {assetData.map((data, index) => (
+                  <Card key={index} className="media-card ">
+                    <Card.Img
+                      className="card-img mx-auto"
+                      alt="cryptocurrency"
+                      variant="top"
+                      src={getCryptoIcon(data.asset)}
+                    />
+                    <Card.Body className="">
+                      <div className="card-grid">
+                        <div className="value-type mx-auto">
+                          <p className="card-text">Asset</p>
+                          <p className="card-text">Qty</p>
+                          <p className="card-text">Spot</p>
+                          <p className="card-text">Value</p>
+                          <p className="card-text">Day</p>
+                          <p className="card-text">Week</p>
+                          <p className="card-text">Month</p>
+                        </div>
+                        <div className="value mx-auto">
+                          <p className="card-text">
+                            {data.asset.toUpperCase()}
+                          </p>
+                          <p className="card-text">
+                            {Number.parseFloat(data.remaining).toFixed(3)}
+                          </p>
+                          <p className="card-text">$ 1.12</p>
+                          <p className="card-text">$ 2.24</p>
+                          <p className="card-text">1.32%</p>
+                          <p className="card-text">0.32%</p>
+                          <p className="card-text">0.01%</p>
+                        </div>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                ))}
+              </Row>
+            </>
+          )}
     </>
   );
 }
