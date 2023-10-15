6c4898033b29a914b56150aee371e7f7f4af706d
Todd <thickam@outlook.com>
2023-10-15 14:15:07 -0700

    updating Card Component for horizontal scroll

--- react_client/src/components/card/Card.jsx
+++ react_client/src/components/card/Card.jsx
@@ -3,55 +3,54 @@
  * Bootstrap 5.0 Grid: https://react-bootstrap.netlify.app/docs/layout/grid/#setting-column-widths-in-row
  */
 import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import crypto_image from "../../assets/crypto.svg";
 
-// CSS Styles (using grid for Card.Text > children)
+// CSS Styles
 import "./styles/card.css";
+
+// Fetch crypto icon using 'asset' as the asset name
+const getCryptoIcon = function fetchCryptoImagePngIcon(asset) {
+  return `https://lcw.nyc3.cdn.digitaloceanspaces.com/production/currencies/64/${asset}.png`;
+};
 
 export default function ComponentOne() {
   return (
     <>
       <h1 className="card-title d-none">Crypto Assets</h1>
-      <div className="card-container">
-        <Row className="media-cards">
-          {Array.from({ length: 12 }).map((_, index) => (
-            <Col key={index}>
-              <Card className="media-element">
-                <Card.Img
-                  className="card-img"
-                  alt="cryptocurrency"
-                  variant="top"
-                  src={crypto_image}
-                />
-                <Card.Body>
-                  <div className="card-grid">
-                    <div className="value-type">
-                      <p className="card-text">Asset</p>
-                      <p className="card-text">Qty</p>
-                      <p className="card-text">Spot</p>
-                      <p className="card-text">Value</p>
-                      <p className="card-text">Day</p>
-                      <p className="card-text">Week</p>
-                      <p className="card-text">Month</p>
-                    </div>
-                    <div className="value">
-                      <p className="card-text">BTC</p>
-                      <p className="card-text">1.0000</p>
-                      <p className="card-text">$ 1.12</p>
-                      <p className="card-text">$ 2.24</p>
-                      <p className="card-text">1.32%</p>
-                      <p className="card-text">0.32%</p>
-                      <p className="card-text">0.01%</p>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
+      <Row className="media-row d-flex flex-nowrap overflow-scroll">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <Card key={index} className="media-card ">
+            <Card.Img
+              className="card-imgi w-50 mx-auto pt-2"
+              alt="cryptocurrency"
+              variant="top"
+              src={getCryptoIcon("btc")}
+            />
+            <Card.Body className="">
+              <div className="card-grid justify-content-center">
+                <div className="value-type mx-auto">
+                  <p className="card-text">Asset</p>
+                  <p className="card-text">Qty</p>
+                  <p className="card-text">Spot</p>
+                  <p className="card-text">Value</p>
+                  <p className="card-text">Day</p>
+                  <p className="card-text">Week</p>
+                  <p className="card-text">Month</p>
+                </div>
+                <div className="value mx-auto">
+                  <p className="card-text">BTC</p>
+                  <p className="card-text">1.0000</p>
+                  <p className="card-text">$ 1.12</p>
+                  <p className="card-text">$ 2.24</p>
+                  <p className="card-text">1.32%</p>
+                  <p className="card-text">0.32%</p>
+                  <p className="card-text">0.01%</p>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
+      </Row>
     </>
   );
 }
