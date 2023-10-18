1bc7e87451e835f28c6dcf0f71b83168ab95c2c7
Todd <thickam@outlook.com>
2023-10-17 21:13:21 -0700

    updated useEffect() with setTimeout()

--- react_client/src/components/card/Card.jsx
+++ react_client/src/components/card/Card.jsx
@@ -2,8 +2,7 @@
  * Bootstrap 5.0 Cards: https://react-bootstrap.netlify.app/docs/components/cards/
  * Bootstrap 5.0 Grid: https://react-bootstrap.netlify.app/docs/layout/grid/#setting-column-widths-in-row
  */
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
+// import Card from "react-bootstrap/Card";
 import { useEffect, useState, useRef } from "react";
 import expressQueryAPI from "../../api/expressQueryAPI";
 import lcwCryptoAPI from "../../api/livecoinwatchAPI";
@@ -13,15 +12,16 @@ import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 // Utilities
 import {
   spinAnimation,
-  getCryptoIcon,
-  textColor,
-  setValueToFixed,
-  setPercentageToFixed,
+  // getCryptoIcon,
+  // textColor,
+  // setValueToFixed,
+  // setPercentageToFixed,
   sortHighLow,
 } from "./styles/utility";
 
 // CSS Styles
 import "./styles/card.css";
+import CardBody from "./CardBody";
 
 // Combine userData and cryptoData into one array (GH)
 const combineData = function combineDataWithCryptoData(
@@ -50,41 +50,36 @@ const combineData = function combineDataWithCryptoData(
     });
   });
   setData(sortHighLow(temp));
-  setAnimation(false);
 };
 
 export default function Cards() {
   const [userData, setUserData] = useState([]);
-  const [expressData, setExpressData] = useState([]);
   const [isData, setIsData] = useState(false);
   const [animation, setAnimation] = useState(true);
   const cardRef = useRef(null); // reference to the card carousel left/right btns
+  const [runEffect, setRunEffect] = useState(true);
 
   const [cardCarousel, setCardCarousel] = useState([]);
 
   // fetch expressQueryAPI and lcwCryptoAPI data, then combine data and set state
   useEffect(() => {
-    console.log("Inside UseEffect()")
+    console.log("Inside UseEffect()");
     async function fetchData() {
       const userData = await expressQueryAPI("remaining");
       const cryptoData = await lcwCryptoAPI();
       if (userData && cryptoData) {
-        setExpressData(userData);
         combineData(userData, cryptoData, setUserData, setAnimation);
         setIsData(true);
         setAnimation(false);
       }
     }
     fetchData();
-  }, []);
+  }, [runEffect]);
 
-  // fetch updated lcwCryptoAPI data every 60 seconds
-  setTimeout(async () => {
-    const cryptoData = await lcwCryptoAPI();
-    if (expressData && cryptoData) {
-      combineData(expressData, cryptoData, setUserData, setAnimation);
-    }
-  }, 180000); // timer set to 3 minutes
+  setTimeout(() => {
+    if (runEffect) setRunEffect(false);
+    else setRunEffect(true);
+  }, 180000); // timer set to 3 seconds
 
   const scrollCardLeft = () => {
     const sLeft = (cardRef.current.scrollLeft -= 175); // adjust the scroll value as needed
@@ -109,57 +104,7 @@ export default function Cards() {
         : isData && (
             <>
               <h1 className="card-title d-none">Crypto Assets</h1>
-              <Row className="media-row d-flex flex-nowrap" ref={cardRef}>
-                {userData.map((data, index) => (
-                  <Card key={index} className="media-card ">
-                    <Card.Img
-                      className="card-img mx-auto"
-                      alt="cryptocurrency"
-                      variant="top"
-                      src={getCryptoIcon(data.asset)}
-                    />
-                    <Card.Body className="card-body">
-                      <p className="card-text text-center fs-5">
-                        {data.asset.toUpperCase()}
-                      </p>
-                      <div className="card-text-body">
-                        <div className="value-type">
-                          <p className="card-text">Qty</p>
-                          <p className="card-text">Spot</p>
-                          <p className="card-text">Value</p>
-                          <p className="card-text">Hour</p>
-                          <p className="card-text">Day</p>
-                          <p className="card-text">Week</p>
-                          <p className="card-text">Month</p>
-                        </div>
-                        <div className="value">
-                          <p className="card-text p">
-                            {setValueToFixed(data.remaining)}
-                          </p>
-                          <p className="card-text p">
-                            ${setValueToFixed(data.spot)}
-                          </p>
-                          <p className="card-text p">
-                            ${setValueToFixed(data.value)}
-                          </p>
-                          <p className={`card-text p ${textColor(data.hour)}`}>
-                            {setPercentageToFixed(data.hour)}%
-                          </p>
-                          <p className={`card-text p ${textColor(data.day)}`}>
-                            {setPercentageToFixed(data.day)}%
-                          </p>
-                          <p className={`card-text p ${textColor(data.week)}`}>
-                            {setPercentageToFixed(data.week)}%
-                          </p>
-                          <p className={`card-text p ${textColor(data.month)}`}>
-                            {setPercentageToFixed(data.month)}%
-                          </p>
-                        </div>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                ))}
-              </Row>
+              <CardBody userData={userData} cardRef={cardRef} />
               <BiLeftArrow className="arrow-left " onClick={scrollCardLeft} />
               <BiRightArrow
                 className="arrow-right "
--- react_client/src/components/card/CardBody.jsx
+++ react_client/src/components/card/CardBody.jsx
@@ -1,5 +1,64 @@
+/* eslint-disable react/prop-types */
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
 
+// Utilities
+import {
+  // spinAnimation,
+  getCryptoIcon,
+  textColor,
+  setValueToFixed,
+  setPercentageToFixed,
+  // sortHighLow,
+} from "./styles/utility";
 
-export default function CardBody() {
-
+// Continue getting incorrect errors for 'props'
+export default function CardBody({userData, cardRef}) {
+  return (
+    <Row className="media-row d-flex flex-nowrap" ref={cardRef}>
+      {userData.map((data, index) => (
+        <Card key={index} className="media-card ">
+          <Card.Img
+            className="card-img mx-auto"
+            alt="cryptocurrency"
+            variant="top"
+            src={getCryptoIcon(data.asset)}
+          />
+          <Card.Body className="card-body">
+            <p className="card-text text-center fs-5">
+              {data.asset.toUpperCase()}
+            </p>
+            <div className="card-text-body">
+              <div className="value-type">
+                <p className="card-text">Qty</p>
+                <p className="card-text">Spot</p>
+                <p className="card-text">Value</p>
+                <p className="card-text">Hour</p>
+                <p className="card-text">Day</p>
+                <p className="card-text">Week</p>
+                <p className="card-text">Month</p>
+              </div>
+              <div className="value">
+                <p className="card-text p">{setValueToFixed(data.remaining)}</p>
+                <p className="card-text p">${setValueToFixed(data.spot)}</p>
+                <p className="card-text p">${setValueToFixed(data.value)}</p>
+                <p className={`card-text p ${textColor(data.hour)}`}>
+                  {setPercentageToFixed(data.hour)}%
+                </p>
+                <p className={`card-text p ${textColor(data.day)}`}>
+                  {setPercentageToFixed(data.day)}%
+                </p>
+                <p className={`card-text p ${textColor(data.week)}`}>
+                  {setPercentageToFixed(data.week)}%
+                </p>
+                <p className={`card-text p ${textColor(data.month)}`}>
+                  {setPercentageToFixed(data.month)}%
+                </p>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+      ))}
+    </Row>
+  );
 }
