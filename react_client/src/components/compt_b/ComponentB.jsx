b49e47f3ca2b96ce2c1cd48f635d812a42fa81d8
Nathan Moes <nathanmoes7@gmail.com>
2023-10-21 18:57:31 -0700

    hope you like doughnuts "joke"

--- react_client/src/components/compt_b/ComponentB.jsx
+++ react_client/src/components/compt_b/ComponentB.jsx
@@ -1,9 +1,27 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 export default function ComponentThree() {
+
+  ChartJS.register(ArcElement, Tooltip, Legend);
+  const data = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3],
+        backgroundColor: ["red", "blue", "yellow"],
+        borderColor: ["red", "blue", "yellow"],
+        borderWidth: 1,
+      },
+    ],
+  }
+
   return (
     <>
       <div className="component-three text-center mt-3">
-        <h1>COMPONENT B</h1>
+        <h1>CHARTS CHARTS CHARTS!!!!!</h1>
       </div>
+      <Doughnut data={data} />
     </>
   );
 }
