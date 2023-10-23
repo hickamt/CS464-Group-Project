8d43d040991de4c5632ee88755d5c6a3ff3b773d
Todd <thickam@outlook.com>
2023-10-23 16:45:26 -0700

    updating notes for arraySort.js

--- react_client/src/modules/arraySort.js
+++ react_client/src/modules/arraySort.js
@@ -1,12 +1,12 @@
 
-// Sort array from greatest to least (GH)
+// Sort array from greatest to least based on object.value
 export const sortValueHL = function sortArrayFromHighToLowByValue(array) {
   return array.sort((a, b) => {
     return b.value - a.value;
   });
 };
 
-// Sort array from least to greatest (GH)
+// Sort array from least to greatest based on object.value
 export const sortValueLH = function sortArrayFromLowtoHighByValue(array) {
   return array.sort((a, b) => {
     return a.value - b.value;
