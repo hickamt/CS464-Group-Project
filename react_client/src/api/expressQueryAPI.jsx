2e890d6eac9360488a24d25343a2f77063159f1a
todd <thickam@outlook.com>
2023-11-13 19:39:41 -0800

    updating allowed origin in expressQueryAPI

--- react_client/src/api/expressQueryAPI.jsx
+++ react_client/src/api/expressQueryAPI.jsx
@@ -25,6 +25,7 @@ export default async function expressQueryAPI(query) {
       .post("/api/queries", {
         headers: {
           "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
         },
         data: {
           query: query,
