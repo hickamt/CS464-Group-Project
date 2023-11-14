661559d6f062f399e9de85bda5e5a46ecab770d8
todd <thickam@outlook.com>
2023-11-13 20:00:48 -0800

    removed some comments

--- react_client/src/api/expressQueryAPI.jsx
+++ react_client/src/api/expressQueryAPI.jsx
@@ -1,24 +1,5 @@
 import axios from "axios";
 
-/*
-  Post request to 'get' one of the following queries:
-  'purchases', 'sales', 'remaining', 'exchanges', or 'assets' 
-
-  Server Request Path: '/express_server/controllers/queriesController.js'
-*/
-
-// const [myData, setmyData] = useState([]);
-// const [isData, setIsData] = useState(false);
-// const [isAnimation, setisAnimation] = useState(false);
-
-// return (
-//   {isAnimation && <MyAnimation />}
-//   {
-
-//     isData && <Mycomponent />
-//   }
-// )
-
 export default async function expressQueryAPI(query) {
   try {
     return await axios
