1f5c0675012b074b1d7cc643e2e155162849c69f
todd <thickam@outlook.com>
2023-11-17 20:45:34 -0800

    trying some shit

--- react_client/vite.config.js
+++ react_client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5523',
+        target: 'https://localhost:5523',
         changeOrigin: true,
       }
     }
