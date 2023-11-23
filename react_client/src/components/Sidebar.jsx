5009173a1fcc7bb283570469509245942b70e49e
todd <thickam@outlook.com>
2023-11-23 01:39:11 -0800

    added font icons for sidebar, styled the sidebar, and resolved icon errors

--- react_client/src/components/Sidebar.jsx
+++ react_client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import { FaChartPie, FaChartLine, FaTable } from "react-icons/fa";
-// import { FcAreaChart } from "react-icons/fc";
+import { FaRegListAlt } from "react-icons/fa";
+import { FcDataSheet, FcDoughnutChart, FcComboChart } from "react-icons/fc";
 import { useAuth } from "../context(s)/AuthContext";
 import pageLogo from "../assets/abstract_c.png";
 // Sidbar setup for testing
@@ -8,53 +8,56 @@ export default function Sidebar({ switchAssetView, switchChartView }) {
   const { currentUser, login, logout } = useAuth();
   return (
     <div className="sidebar text-center">
-      <img src={pageLogo} alt="abstract c" className="sidebar-image d-flex flex-column align-items-center"></img>
+      <img
+        src={pageLogo}
+        alt="abstract c"
+        className="sidebar-image d-flex flex-column align-items-center"></img>
       <section className="sidebar-header-space"></section>
       <section className="sidebar-chart-view">
-        <p className="sidebar-charts fs-5 mt-1">VISUALIZE</p>
+        {/* <p className="sidebar-charts fs-5 mt-1">VISUALIZE</p> */}
         <button
-          className="card-btn rounded m-1"
+          className="card-btn sidebar-btn"
           value="chart"
-          onClick={(e) => switchChartView(e, e.target.value)}
-        >
-          <FaChartPie />
-          Pie Chart
+          aria-label="Pie Chart"
+          onClick={(e) => switchChartView(e, e.target.value)}>
+          <FcDoughnutChart className="fc-icon" />
+          {/* Pie Chart */}
         </button>
         <button
-          className="card-btn rounded m-1"
+          className="card-btn sidebar-btn"
           value="line-chart"
-          onClick={(e) => switchChartView(e, e.target.value)}
-        >
-          <FaChartLine />
-          Line Chart
+          aria-label="Line Chart"
+          onClick={(e) => switchChartView(e, e.target.value)}>
+          <FcComboChart className="fc-icon" />
+          {/* Line Chart */}
         </button>
         <button
-          className="card-btn rounded m-1"
+          className="card-btn sidebar-btn"
           value="line-compare"
-          onClick={(e) => switchChartView(e, e.target.value)}
-        >
-          <FaChartLine />
-          Compare
+          aria-label="Line Compare"
+          onClick={(e) => switchChartView(e, e.target.value)}>
+          <FcComboChart className="fc-icon" />
+          {/* Compare */}
         </button>
       </section>
 
       <section className="sidebar-asset-view">
-        <p className="sidebar-analyze fs-5 mt-1">ANALYZE</p>
+        {/* <p className="sidebar-analyze fs-5 mt-1">ANALYZE</p> */}
         <button
-          className="card-btn rounded m-1"
+          className="card-btn sidebar-btn"
           value="card"
-          onClick={(e) => switchAssetView(e, e.target.value)}
-        >
-          <FaTable />
-          Card
+          aria-label="Price Card"
+          onClick={(e) => switchAssetView(e, e.target.value)}>
+          <FaRegListAlt className="fc-icon" />
+          {/* Card */}
         </button>
         <button
-          className="card-btn rounded m-1"
+          className="card-btn sidebar-btn"
           value="table"
-          onClick={(e) => switchAssetView(e, e.target.value)}
-        >
-          <FaTable />
-          Table
+          aria-label="Data Table"
+          onClick={(e) => switchAssetView(e, e.target.value)}>
+          <FcDataSheet className="fc-icon" />
+          {/* Table */}
         </button>
       </section>
       <section className="login-logout mt-auto">
